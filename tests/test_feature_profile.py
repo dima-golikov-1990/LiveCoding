a67cccb8c9357ffcac2fc2b1572f97bb68a38d82
dima-golikov-1990 <145390552+dima-golikov-1990@users.noreply.github.com>
2024-03-16 20:06:41 +0300

    First CI push

---
+++ tests/test_feature_profile.py
@@ -0,0 +1,23 @@
+import allure
+from base.base_test import BaseTest
+
+@allure.feature("Profile Functionality")
+class TestProfileFeature(BaseTest):
+
+    @allure.title("Change profile name")
+    @allure.severity("Critical")
+    def test_change_profile_name(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit_button()
+
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info_link()
+
+        self.personal_page.is_opened()
+        self.personal_page.change_name("dfxfdfg")
+        self.personal_page.save_changes()
+        self.personal_page.is_changes_saved()
+
+
